547e3603bb7467725ee855f7fd9428a14e897a87
vishneuski <vishneuski87@gmail.com>
2019-03-09 16:53:33 +0300

    Create manifest.json and work with gallery-slider.js

--- scripts/loft-gallery-slider.js
+++ scripts/loft-gallery-slider.js
@@ -1,35 +1,29 @@
 $(document).ready(function () {
 
-  //Create variables
-
   var $item = $('.slider__list-item');
-  var $next = $('.slider-button-next');
-  var $prev = $('.slider-button-prev');
-  var index;
+  var $next = $('.slider__button-next');
+  var $prev = $('.slider__button-prev');
 
-  //Add class .active to first $item.
   $item.first().addClass('active');
 
-  //Add click event to button NEXT
+  $next.click(function() {
+    var index = $item.filter('.active').index();
 
-  $next.click(function () {
-    index = $item.filter('.active').index();
-
-  //Check index. Do loop.
-
-    if(index == $item.length - 1){
-      index = -1;
+    if (index === $item.length - 1) {
+        index = -1;
     }
+
     $item.eq(index + 1).addClass('active').siblings().removeClass('active');
   });
 
-  $prev.click(function () {
+  $prev.click(function() {
     var index = $item.filter('.active').index();
 
-    if(index == 0) {
-      index = $item.length;
+    if (index === 0) {
+        index = $item.length;
     }
 
     $item.eq(index - 1).addClass('active').siblings().removeClass('active');
-  })
+  });
 });
+
