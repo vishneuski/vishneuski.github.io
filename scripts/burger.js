6d3367dd16f85a58ea007ab5d137cc731540be40
vishneuski <vishneuski87@gmail.com>
2019-03-16 08:30:14 +0300

    Fix some bugs to correct work sticky menu of all pages

--- scripts/burger.js
+++ scripts/burger.js
@@ -1,34 +1,26 @@
-$(document).ready(function(){
-  $('#burger').click(function(){
+$(document).ready(function () {
+  $('#burger').click(function () {
     $('.navigation').toggleClass('active');
     $('.burger-icon-item').toggleClass('rotate');
   });
 
   $(window).scroll(function () {
-      if($(document).scrollTop() > 300){
-        $('.navigation').removeClass('active');
-        $('.burger-icon-item').removeClass('rotate');
-      }
-  });
-
-  $(window).scroll(function () {
-    if($(document).scrollTop() > 300) {
-      $('.loft-header').addClass('sticky');
-      $('.loft-logo').addClass('sticky');
-
-    } else {
-      $('.loft-header').removeClass('sticky');
-      $('.loft-logo').removeClass('sticky');
+    if ($(document).scrollTop() > 300) {
+      $('.navigation').removeClass('active');
+      $('.burger-icon-item').removeClass('rotate');
     }
   });
 
   $(window).scroll(function () {
-    if($(document).scrollTop() > 300) {
-      $('.navigation-layout').addClass('sticky');
+    if ($(document).scrollTop() > 300) {
+      $('.loft-header, .loft-logo, .navigation-layout').addClass('sticky');
+
     } else {
-      $('.navigation-layout').removeClass('sticky');
+      $('.loft-header, .loft-logo, .navigation-layout').removeClass('sticky');
     }
   });
 });
+
+
 
 
